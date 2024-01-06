fbd8a1dbf089728f2f091ac4e6f32f06288f9a4e
simondrews <simondrews.sd@gmail.com>
2024-01-06 20:55:48 +0100

    Other approach at searchbar with a div

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import styles from "./Searchbar.module.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
 const Searchbar: React.FC = () => {
     return (
-        <>
+        <div className={styles.container}>
             <input 
                 type="search"
                 placeholder="Search a location..."
                 className={styles.search}
             />
-        </>
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+        </div>
     );
 };
 
