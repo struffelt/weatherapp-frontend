92a315826fc4553712540a688b287ce1f98045c4
simondrews <simondrews.sd@gmail.com>
2024-01-07 21:20:34 +0100

    Layout and added Weather as a plain div

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import { useState } from 'react'
-import './App.css'
-import Searchbar from './components/Searchbar'
+import { useState } from 'react';
+import styles from './App.module.css';
+import Searchbar from './components/Searchbar';
+import Weather from './components/Weather';
 
 function App() {
   const [search, setSearch] = useState<string>("");
 
   return (
-    <>
-      <Searchbar search={search} setSearch={setSearch}/>
-    </>
-  )
-}
+    <div className={styles.container}>
+      <Searchbar search={search} setSearch={setSearch} />
+      <Weather />
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Weather.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Weather.module.css";
+
+const Weather: React.FC = () => {
+    return (
+        <div className={styles.container}>
+
+        </div>
+    );
+};
+
+export default Weather;
